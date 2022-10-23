72727c6a4d0c8e1072b07ae3484a7363731d71dd
Falseclock <nurike@gmail.com>
2022-10-23 13:13:27 +0600

    htmlSpecialChars

--- src/Utils/TextUtils.php
+++ src/Utils/TextUtils.php
@@ -17,6 +17,33 @@ final class TextUtils
 {
     const EMAIL_CHECK_REGEXP = '/^(?!(?:(?:\x22?\x5C[\x00-\x7E]\x22?)|(?:\x22?[^\x5C\x22]\x22?)){255,})(?!(?:(?:\x22?\x5C[\x00-\x7E]\x22?)|(?:\x22?[^\x5C\x22]\x22?)){65,}@)(?:(?:[\x21\x23-\x27\x2A\x2B\x2D\x2F-\x39\x3D\x3F\x5E-\x7E]+)|(?:\x22(?:[\x01-\x08\x0B\x0C\x0E-\x1F\x21\x23-\x5B\x5D-\x7F]|(?:\x5C[\x00-\x7F]))*\x22))(?:\.(?:(?:[\x21\x23-\x27\x2A\x2B\x2D\x2F-\x39\x3D\x3F\x5E-\x7E]+)|(?:\x22(?:[\x01-\x08\x0B\x0C\x0E-\x1F\x21\x23-\x5B\x5D-\x7F]|(?:\x5C[\x00-\x7F]))*\x22)))*@(?:(?:(?!.*[^.]{64,})(?:(?:(?:xn--)?[a-z0-9]+(?:-[a-z0-9]+)*\.){1,126}){1,}(?:(?:[a-z][a-z0-9]*)|(?:(?:xn--)[a-z0-9]+))(?:-[a-z0-9]+)*)|(?:\[(?:(?:IPv6:(?:(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){7})|(?:(?!(?:.*[a-f0-9][:\]]){7,})(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,5})?::(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,5})?)))|(?:(?:IPv6:(?:(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){5}:)|(?:(?!(?:.*[a-f0-9]:){5,})(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,3})?::(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,3}:)?)))?(?:(?:25[0-5])|(?:2[0-4][0-9])|(?:1[0-9]{2})|(?:[1-9]?[0-9]))(?:\.(?:(?:25[0-5])|(?:2[0-4][0-9])|(?:1[0-9]{2})|(?:[1-9]?[0-9]))){3}))\]))$/iD';
 
+    public static function htmlSpecialChars($data) {
+        if(is_array($data)) {
+            foreach($data as $key => $value) {
+                $data[htmlspecialchars($key)] = TextUtils::htmlSpecialChars($value);
+            }
+        }
+        else if(is_object($data)) {
+            $values = get_class_vars(get_class($data));
+            foreach($values as $key => $value) {
+                $data->{htmlspecialchars($key)} = TextUtils::htmlSpecialChars($value);
+            }
+        }
+        else {
+            switch(gettype($data)) {
+                case "boolean":
+                case "integer":
+                case "double":
+                case "float":
+                case "NULL":
+                    break;
+                default:
+                    $data = htmlspecialchars($data);
+            }
+        }
+
+        return $data;
+    }
     /**
      * Проверка email адресам на RFC валидность
      *
